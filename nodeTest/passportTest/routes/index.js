d91463f3e6e1908d707f28c1d07b0385ec202b1f
yanjixian <yjx3097890@sina.com>
2015-05-26 23:02:27 +0800

    d3

--- nodeTest/passportTest/routes/index.js
+++ nodeTest/passportTest/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  
+   if (!req.signedCookies.name) {
+        res.cookie('name', 'tobi',  { signed: true });
+   }
+   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
